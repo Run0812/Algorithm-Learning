95349ed9a5b4b96096564e796133f9f1d735c3ad
TianRun0812 <traft@126.com>
2019-01-06 21:36:01 +0800

    2019.01.06 LAB

--- to_offer/19_RegularExpressionsMatching.py
+++ to_offer/19_RegularExpressionsMatching.py
@@ -1,4 +1,9 @@
-
+"""
+面试题19：正则表达式匹配
+题目：请实现一个函数来匹配包含'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符
+可以出现任意次（含0次）。在本题中，匹配是指字符串中的所有字符匹配整个模式。
+例如，字符串"aaa"与模式"a.a"和“ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
+"""
 def re_exp_matching_backward(s, p):
     """
     :type s: str for match
@@ -56,9 +61,26 @@ def re_exp_matching(s, p):
 
 def re_exp_matching_dp(s, p):
 
-    return
+    col, row = len(p), len(s)
+    dp = [[0]* (col+1) for _ in range(row+1)]
+    dp[0][0] = 1
+    for i in range(2, col+1):
+        if p[i-1] == '*':
+            dp[0][i] = dp[0][i-2]
 
-s = "aa"
+    for i in range(1, row + 1):
+        for j in range(1, col + 1):
+            if p[j-1] == '*':
+                if s[i-1] != p[j-2] and p[j-2] != '.':
+                    dp[i][j] = dp[i][j-2]
+                elif s[i-1] == p[j-2] or p[j-2] == '.':
+                    dp[i][j] = dp[i][j-2] or dp[i-1][j]
+            elif s[i-1] == p[j-1] or p[j-1] == '.':
+                dp[i][j] = dp[i-1][j-1]
 
-p = "a"
-print(re_exp_matching(s,p))
+    return dp[-1][-1] == 1
+
+s = "ab"
+
+p = ".*"
+print(re_exp_matching_dp(s,p))
