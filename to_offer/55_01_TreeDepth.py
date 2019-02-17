95524b860f7482b56c34f3ba4938621c7782edf2
TianRun0812 <traft@126.com>
2019-02-17 13:25:29 +0800

    2019.02.17 LAB

--- to_offer/55_01_TreeDepth.py
+++ to_offer/55_01_TreeDepth.py
@@ -1,3 +1,28 @@
 """
-面试题55:
+面试题55:二叉树的深度
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点一次经过的节点
+形成树的一条路径，从最长路径的长度为树的深度。
+
+例：
+        1
+       / \
+      2   3
+     / \   \
+    4   5   6
+       /
+      7
+深度为4。
 """
+
+
+def depth_of_tree(root):
+
+    if not root:
+        return 0
+    left = depth_of_tree(root.left)
+    right = depth_of_tree(root.right)
+    return max(left, right) + 1
+
+from datstru import list_to_treenode
+tree = list_to_treenode([1,2,3,4,5,'null',6,'null','null',7])
+print(depth_of_tree(tree))
