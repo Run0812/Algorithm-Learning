e28aa86db79b52d4886e38900d05956dc7871e2b
TianRun0812 <traft@126.com>
2019-02-18 17:52:28 +0800

    2019.02.18 LAB

---
+++ to_offer/61_ContinuousCards.py
@@ -0,0 +1,26 @@
+"""
+面试题61：扑克牌中的顺子
+题目：从扑克牌中随机抽5张牌，判断是不是一个顺子，即5张牌是不是连续的。
+2~10为数字本身，A为1，J为11，Q为12，K为13，而大、小王可以看成任意数字。
+"""
+
+
+def continuous_cards(cards):
+    cards.sort()
+    joker = 0
+    for card in cards:
+        if card == 0:
+            joker += 1
+        else:
+            break
+    if joker > 2:
+        raise Exception('impossible combo')
+    for i in range(joker+1,5):
+        if cards[i] - cards[i - 1] != 1:
+            if joker:
+                joker -= 1
+            else:
+                return False
+    return True
+
+print(continuous_cards([ 1, 2, 3, 4, 0]))
