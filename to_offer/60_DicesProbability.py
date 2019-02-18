f0acff6cad51952b752213fa947257b6c7523fbf
TianRun0812 <traft@126.com>
2019-02-18 17:25:05 +0800

    2019.02.18 LAB

---
+++ to_offer/60_DicesProbability.py
@@ -0,0 +1,23 @@
+"""
+问题60：n个骰子的点数
+题目：把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。
+输入n，打印出s的所有可能的值出现的概率。
+"""
+
+
+def dices_probability(n):
+    times = {1:1, 2:1, 3:1, 4:1, 5:1, 6:1}
+    for i in range(2,n+1):
+        cache = {}
+        for val in range(1 * i, 6 * i + 1):
+            cache[val] = times.get(val - 1, 0) + times.get(val - 2, 0) + times.get(val - 3, 0)\
+                         + times.get(val - 4, 0) + times.get(val - 5, 0) + times.get(val - 6, 0)
+        times = cache
+
+    for val in times:
+        from fractions import Fraction
+        times[val] = Fraction(times[val], 6 ** n)
+        # times[val] = times[val] / 6 ** n
+    return times
+
+print(dices_probability(11))
